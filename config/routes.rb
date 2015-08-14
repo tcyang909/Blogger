0e23aab8ae40ab2e18cd43c47c788300c9158181
timothy yang <tim.c.yang@gmail.com>
2015-08-13 17:05:55 -0700

    form-based workflow feature completed

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-     resources :articles
+    root to: 'articles#index'
+    resources :articles
 
   # Example resource route with options:
   #   resources :products do
