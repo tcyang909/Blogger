c404869328240dc4c229a73dcf3693e3c69c6875
timothy yang <tim.c.yang@gmail.com>
2015-08-17 19:55:51 -0700

    Sorcery authentication complete

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,12 @@ Rails.application.routes.draw do
       resources :comments
     end
     resources :tags
+    resources :authors
+
+    resources :author_sessions, only: [:new, :create, :destroy]
+
+    get 'login' => 'author_sessions#new'
+    get 'logout' => 'author_sessions#destroy'
 
   # Example resource route with options:
   #   resources :products do
